3b23abe8505b08b3f8501288edf0d0239cb2f57e
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2022-09-11 15:56:29 +0600

    object

--- 1.Types/5_function.js
+++ 1.Types/5_function.js
@@ -1,38 +1,28 @@
 // 1. function with parameter
-const add = (num1: number, num2: number) => {
-  console.log("result :", num1 + num2);
+var add = function (num1, num2) {
+    console.log("result :", num1 + num2);
 };
-
 add(2, 5);
-
 // 2. function with optional parameter
-const show = (num1: number, num2?: number) => {
-  console.log("result :", num1, num2);
+var show = function (num1, num2) {
+    console.log("result :", num1, num2);
 };
-
 show(12);
 show(12, 23);
-
 // 3. function return type
-
 // 3.1 return void
-const showName = (): void => {
-  console.log("Md Mohimenol Islam Munna");
+var showName = function () {
+    console.log("Md Mohimenol Islam Munna");
 };
-
 showName();
-
 // // 3.2 return void
 // const showName2 = (): never => {
 //   console.log("Md Mohimenol Islam Munna");
 // };
-
 // showName2();
-
 // 3.3 return others type
-const showName3 = (): string => {
-  console.log("Md Mohimenol Islam Munna 2");
-  return "Md";
+var showName3 = function () {
+    console.log("Md Mohimenol Islam Munna 2");
+    return "Md";
 };
-
 console.log(showName3());
