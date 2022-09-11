a456b0ef42bf5a43575d48a987b291925ed8d974
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2022-09-11 16:54:00 +0600

    function type

--- 1.Types/5_function.js
+++ 1.Types/5_function.js
@@ -1,3 +1,19 @@
+// accept all function
+var multiply1;
+var multiplyFunc1 = function (a, b) {
+    return a * b;
+};
+multiply1 = multiplyFunc1;
+console.log("result :1:", multiply1(10, 6));
+function printResult() {
+    console.log("print result function ::");
+}
+// accept function with the specific type
+var multiply2;
+multiply2 = multiplyFunc1;
+console.log("multiply2 result ::", multiply2(10, 2, 3));
+// multiply2 = printResult; through error
+// console.log("multiply2 result ::", multiply2(2, 3, 4));
 // 1. function with parameter
 var add = function (num1, num2) {
     console.log("result :", num1 + num2);
--- 1.Types/5_function.ts
+++ 1.Types/5_function.ts
@@ -1,3 +1,26 @@
+// accept all function
+let multiply1: Function;
+
+const multiplyFunc1 = (a: number, b: number): number => {
+  return a * b;
+};
+
+multiply1 = multiplyFunc1;
+console.log("result :1:", multiply1(10, 6));
+
+function printResult() {
+  console.log("print result function ::");
+}
+
+// accept function with the specific type
+let multiply2: (p: number, q: number, r: number) => number;
+
+multiply2 = multiplyFunc1;
+console.log("multiply2 result ::", multiply2(10, 2, 3));
+
+// multiply2 = printResult; through error
+// console.log("multiply2 result ::", multiply2(2, 3, 4));
+
 // 1. function with parameter
 const add = (num1: number, num2: number) => {
   console.log("result :", num1 + num2);
