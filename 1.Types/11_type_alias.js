53df526881bab1346875bb224ba793ff66c45fbd
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2022-09-11 19:38:22 +0600

    type alias

---
+++ 1.Types/11_type_alias.js
@@ -0,0 +1,17 @@
+// 11. Type Alias (Custom Type)
+// variable with type alias
+var playerIncom;
+playerIncom = "120 core";
+console.log("playerIncom ::", playerIncom);
+// object with type alias
+var player1 = {
+    id: "m-13",
+    name: "messi",
+    club: "psg",
+    country: "argentina",
+    hasWc: false
+};
+console.log("player1 ::", player1);
+// array with type alias
+var countries = ["bd", 880, "ind"];
+console.log("countries ::", countries);
---
+++ 1.Types/11_type_alias.ts
@@ -0,0 +1,35 @@
+// 11. Type Alias (Custom Type)
+
+type CountryOrCountryCode = string | number;
+
+type PlayerIncome = string | number;
+
+type Player = {
+  id: string;
+  name: string;
+  club: string;
+  country: string;
+  hasWc: boolean;
+};
+
+// variable with type alias
+let playerIncom: PlayerIncome;
+playerIncom = "120 core";
+console.log("playerIncom ::", playerIncom);
+
+// object with type alias
+const player1: Player = {
+  id: "m-13",
+  name: "messi",
+  club: "psg",
+  country: "argentina",
+  hasWc: false,
+};
+
+console.log("player1 ::", player1);
+
+// array with type alias
+let countries: CountryOrCountryCode[] = ["bd", 880, "ind"];
+
+console.log("countries ::", countries);
+
