d33b272b2c32d8d62fe9aac534d6e850b5988d37
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2022-09-11 19:02:21 +0600

    type infer

---
+++ 1.Types/8_type_infer.js
@@ -0,0 +1,8 @@
+// 8. Type Infer : Typescript can infer the type of variable based on it's value. 
+// here we asign string value to playerName variable 
+var playerName = "Tamim";
+// These will through error cause typescript infered that, type of playerName variable is string based on it's initial value.
+// playerName = 10; 
+// playerName = true;
+playerName = "Tamim Iqbal Khan";
+console.log("player name ::", playerName);
